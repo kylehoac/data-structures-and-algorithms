572eaab80b486208a79d01929bdc33f5d16ce86c
Kyle Hoac <hoackyle@gmail.com>
2021-04-21 23:28:20 -0700

    finished 1-6

--- code-challenges/challenges-08.test.js
+++ code-challenges/challenges-08.test.js
@@ -115,7 +115,7 @@ const isCapitalized = (str) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
 
-Write a function named citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
+Write a function namd citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
 ------------------------------------------------------------------------------------------------ */
 
 const citiesAtoJ = (arr) => {
